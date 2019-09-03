664974246839edbd7a1c36d78e20ac467331205a
trallerd <jessicacristec@gmail.com>
2019-09-03 13:36:50 -0300

    chico game

--- chico/src/model/Jogadas.java
+++ chico/src/model/Jogadas.java
@@ -1,4 +1,48 @@
 package model;
 
-public class Jogadas {
+import java.io.Serializable;
+
+public class Jogadas implements Serializable {
+    private Jogador jogador1, jogador2;
+    private long palpite1, palpite2, soma;
+
+    public Jogador getJogador1() {
+        return jogador1;
+    }
+
+    public void setJogador1(Jogador jogador1) {
+        this.jogador1 = jogador1;
+    }
+
+    public Jogador getJogador2() {
+        return jogador2;
+    }
+
+    public void setJogador2(Jogador jogador2) {
+        this.jogador2 = jogador2;
+    }
+
+    public long getPalpite1() {
+        return palpite1;
+    }
+
+    public void setPalpite1(long palpite1) {
+        this.palpite1 = palpite1;
+    }
+
+    public long getPalpite2() {
+        return palpite2;
+    }
+
+    public void setPalpite2(long palpite2) {
+        this.palpite2 = palpite2;
+    }
+
+    public long getSoma() {
+        return soma;
+    }
+
+    public void setSoma(long soma) {
+        this.soma = soma;
+    }
 }
--- chico/src/model/Jogador.java
+++ chico/src/model/Jogador.java
@@ -1,4 +1,51 @@
 package model;
 
-public class Jogador {
+import java.io.Serializable;
+
+public class Jogador implements Serializable {
+    private String nome;
+    private String email;
+    private String senha;
+    private long nJogadas;
+    private long pontos;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public long getnJogadas() {
+        return nJogadas;
+    }
+
+    public void setnJogadas(long nJogadas) {
+        this.nJogadas = nJogadas;
+    }
+
+    public long getPontos() {
+        return pontos;
+    }
+
+    public void setPontos(long pontos) {
+        this.pontos = pontos;
+    }
 }
